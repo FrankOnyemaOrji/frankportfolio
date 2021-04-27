d5ddc6e85c356e8e45899bafa9211e012f7f3018
Frank Orji <orjifrank722@gmail.com>
2021-04-27 10:49:01 -0700

    Changes made for portfolio to look more good and i fell happy about it

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,126 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(PortFolio());
-}
-
+void main() => runApp(PortFolio());
 class PortFolio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.black87,
         body: SafeArea(
-          child: Column(
-            children: [
-              CircleAvatar(
-
-              )
-            ],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                  CircleAvatar(
+                   radius: 150.0,
+                   backgroundImage: AssetImage('images/onyemauchechi.jpg'),
+                 ),
+                SizedBox(
+                  width: 200,
+                  height: 12,
+                ),
+                Text(
+                  'Frank Orji',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'Flutter Developer',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                    color: Colors.blue.shade900,
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                  child: Text(
+                    'I am a flutter developer who loves to build responsive and Scalable Apps for Android, IOS and Web, I love learning new things and i code_4_life',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                  child: Text(
+                    'EXPERIENCE',
+                    style: TextStyle(
+                      color: Colors.blue.shade900,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.symmetric(vertical: 2, horizontal: 30),
+                  child: Text(
+                    'I am getting there',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Text(
+                    'CONTACT ME',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.blue.shade900,
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading:Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      '+2349065099307',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text(
+                      'frank@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
